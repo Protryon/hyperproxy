4b142ff9bf5572bef827890d8852cd02189b3ac0
Protryon <max.bruce12@gmail.com>
2022-05-06 09:18:14 -0700

    v0.2.7

--- src/lib.rs
+++ src/lib.rs
@@ -333,10 +333,6 @@ impl AsyncRead for WrappedStream {
             Poll::Ready(Err(e)) => {
                 self.fused_error = true;
                 self.pending_read_proxy = None;
-                #[cfg(feature = "tonic")]
-                {
-                    *self.connect_info.write().unwrap() = Some(self.source());
-                }
                 Poll::Ready(Err(e))
             }
             Poll::Ready(Ok((ProxyResult::SignatureBytes(bytes), stream))) => {
